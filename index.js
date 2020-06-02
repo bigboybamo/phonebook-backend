ad5679765492ae08b21f66cfca5ff7589c306215
Bamiji Oyetubo <oyetubobamiji@gmail.com>
2020-06-02 13:58:11 +0100

    added some files

---
+++ index.js
@@ -0,0 +1,100 @@
+const express = require('express');
+
+let app = express();
+app.use(express.json());
+
+let phonebook = [
+
+{
+    name: "Olabamiji Oyetubo",
+    number : "08123577720",
+    id: 1
+},
+{
+    name: "Ann Effiong",
+    number :"0863456789",
+    id: 2
+
+},
+
+{
+    name: "Jerry Smith",
+    number : "081233576720",
+    id: 3
+},
+{
+    name: "Connie Springer",
+    number : "0986523467",
+    id: 4
+
+}
+
+]
+
+app.get('/api/persons',(req,res)=>{
+
+res.json(phonebook);
+
+})
+
+app.get('/info',(req,res)=>{
+
+    const count = phonebook.length;
+    const d = new Date();
+
+    console.log(count);
+    res.send(`The phonebook has info for ${count} people
+   the time this current book was generated was ${d} `)
+    
+})
+
+app.get('/api/persons/:id',(req,res)=>{
+const id = Number(req.params.id);
+const person = phonebook.find( person=> person.id == id );
+
+if (person) {
+    res.json(person)
+  } else {
+    res.status(404).end()
+  }
+})
+
+app.delete('/api/persons/:id',(req,res)=>{
+
+    const id = Number(req.params.id)
+  notes = phonebook.filter(note => note.id !== id)
+
+  res.status(204).end()
+})
+
+
+
+  app.post('/api/persons',(req,res)=>{
+    const body = req.body
+
+  if (body.name === undefined){
+    return response.status(400).json({
+        error: "name missing"
+      });
+  }
+  if(body.number === undefined){
+    return response.status(400).json({
+        error: "number missing"
+      });
+  }
+
+  const person = new phonebook({
+    name: body.name,
+    number: body.number,
+    id: Math.floor(Math.random() * 100000)
+  });
+
+
+
+  })
+
+const port = 3001;
+app.listen(port,()=>{
+    console.log(`Listening on port ${port}`)
+});
+
